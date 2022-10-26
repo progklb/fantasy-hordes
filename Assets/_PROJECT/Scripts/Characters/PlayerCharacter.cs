08da61d8116cdec9dfea9a6a1399911c9a17d906
Kevin Baynham <kevin.baynham.k7@gmail.com>
2022-10-26 08:26:39 +0200

    Added idle and running animations to the player character.
    Idle and running animations have been included (downloaded from Mixamo - see README). A configured player character is now spawned at start up and animations play when running.

--- Assets/_PROJECT/Scripts/Characters/PlayerCharacter.cs
+++ Assets/_PROJECT/Scripts/Characters/PlayerCharacter.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
+using Utilities.Extensions;
+
 using FantasyHordes.Helpers;
 
 namespace FantasyHordes.Characters
@@ -13,15 +15,45 @@ namespace FantasyHordes.Characters
     /// </summary>
     public class PlayerCharacter : MovableCharacter
     {
+        #region PROPERTIES
+        public Animator animator { get; private set; }
+        #endregion
+
+        #region VARIABLES
+        [SerializeField, Tooltip("The transform that will hold the player character model.")]
+        private Transform m_ModelParent;
+		[SerializeField, Tooltip("The rigged/animated character prefab to be used by this instance.")]
+		private GameObject m_PlayerPrefab;
+        #endregion
+
+
         #region UNITY EVENTS
         protected override void Start()
         {
             base.Start();
 
             InputManager.onClick += OnClick;
+            m_ModelParent.DestroyChildren();
+
+			animator = Instantiate(m_PlayerPrefab, m_ModelParent).GetComponent<Animator>();
         }
 
-        protected override void OnDestroy()
+        protected virtual void Update()
+		{
+            // TODO Refine this.
+            // Use an enum to represent states, and migrate this logic
+            // into a proper state machine.
+			if (agent.remainingDistance < 0.2f)
+			{
+				animator.SetBool("Running", false);
+			}
+            else
+			{
+				animator.SetBool("Running", true);
+			}
+		}
+
+		protected override void OnDestroy()
         {
             base.OnDestroy();
 
